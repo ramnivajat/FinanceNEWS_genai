75e0c3c9a1580843e88d9e6fd3151086ea7ec11e
ramnivajat <88840119+ramnivajat@users.noreply.github.com>
2024-07-05 13:45:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ process_url_clicked = st.sidebar.button("Process URLs")
 file_path = "faiss_store_openai.pkl"
 
 main_placeholder = st.empty()
-llm = OpenAI(api_key="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",temperature=0.9, max_tokens=500)
+llm = OpenAI(api_key=os.getenv('OPENAI_API_KEY'),temperature=0.9, max_tokens=500)
 
 if process_url_clicked:
     # load data
